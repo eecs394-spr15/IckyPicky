e5852c645de0f8b220b189fb6582981e44c5412a
Jon Ford <jondavidford@gmail.com>
2015-04-22 12:27:57 -0500

    merged with NosePosition branch, image does not display on canvas when selected

--- app/Yellow/scripts/NosePosController.js
+++ app/Yellow/scripts/NosePosController.js
@@ -16,7 +16,10 @@ angular
             };
   })();
 
-  var NosePoser = {
+  Parse.initialize("r0KuXabCgDMYoC1v62X0D5j3hyvDcEI2IDNSPRJM", "Cux3e19rL6sqTDRunw1WWJWbMAlpY1XYg3FsePFH");
+  var ParseImageObject = Parse.Object.extend("image");
+
+  var NoserPoser = {
 
     // set up some initial values
     WIDTH: 320, 
@@ -43,126 +46,149 @@ angular
     noseHeight: 44,
     drawNose: false,
 
-
-    nImages: 2,
-    nLoadedImages: 0,
-
-    faceImg: new Image,
     noseImg: new Image,
+    faceImg: new Image,
+    
+    userNewImage: new ParseImageObject(),
+    userImageLoaded: false,
 
     init: function() {
 
+      // get an image
+      navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY });
+
+      // save the image along with device id
+      function onSuccess(imageData) {
+        NoserPoser.faceImg.src = imageData;
+        NoserPoser.userImageLoaded = true;
+
+        var bitmap = new Parse.File("someimage.jpg", {base64: "data:image/jpeg;base64," + imageData});
+        NoserPoser.userNewImage.set('bitmap', bitmap);
+        NoserPoser.userNewImage.set('deviceid', device.uuid);
+
+        NoserPoser.userNewImage.save(null, {
+          success: function(userNewImage) {
+            // Execute any logic that should take place after the object is saved.
+            alert('New image created with objectId: ' + userNewImage.id);
+          },
+          error: function(image, error) {
+            // Execute any logic that should take place if the save fails.
+            // error is a Parse.Error with an error code and message.
+            alert('Failed to create new image, with error code: ' + error.message);
+          }
+        });
+      }
+
+      function onFail(message) {
+          alert('Failed to get picture because: ' + message);
+      }
+
 
       // the proportion of width to height
-      NosePoser.RATIO = NosePoser.WIDTH / NosePoser.HEIGHT;
+      NoserPoser.RATIO = NoserPoser.WIDTH / NoserPoser.HEIGHT;
       // these will change when the screen is resized
-      NosePoser.currentWidth = NosePoser.WIDTH;
-      NosePoser.currentHeight = NosePoser.HEIGHT;
+      NoserPoser.currentWidth = NoserPoser.WIDTH;
+      NoserPoser.currentHeight = NoserPoser.HEIGHT;
       // this is our canvas element
-      NosePoser.canvas = document.getElementsByTagName('canvas')[0];
+      NoserPoser.canvas = document.getElementsByTagName('canvas')[0];
       // setting this is important
       // otherwise the browser will
       // default to 320 x 200
-      NosePoser.canvas.width = NosePoser.WIDTH;
-      NosePoser.canvas.height = NosePoser.HEIGHT;
+      NoserPoser.canvas.width = NoserPoser.WIDTH;
+      NoserPoser.canvas.height = NoserPoser.HEIGHT;
       // the canvas context enables us to 
       // interact with the canvas api
-      NosePoser.ctx = NosePoser.canvas.getContext('2d');
+      NoserPoser.ctx = NoserPoser.canvas.getContext('2d');
 
-      // set initial face for first level
-      NosePoser.currentFaceImg = NosePoser.arisaFaceImg;
-      NosePoser.currentHeartImg = NosePoser.heartFullImg;
 
-      // load images
-      NosePoser.faceImg.onload = function() {
-        NosePoser.nLoadedImages += 1;
-        NosePoser.maybeStart();
-
-       } 
-      NosePoser.faceImg.src = '/images/arisaface.png';
-
-      NosePoser.noseImg.onload = function(){
-        NosePoser.nLoadedImages += 1;
-        NosePoser.maybeStart();
+      NoserPoser.noseImg.onload = function(){
+        NoserPoser.loop();
       }
-      NosePoser.noseImg.src = '/images/nose.png';
+      NoserPoser.noseImg.src = '/images/nose.png';
+
+
+      // listen for clicks
+      window.addEventListener('click', function(e) {
+          e.preventDefault();
+          NoserPoser.Input.set(e);
+      }, false);
+      // listen for touches
+      window.addEventListener('touchstart', function(e) {
+          e.preventDefault();
+          // the event object has an array
+          // called touches, we just want
+          // the first touch
+          NoserPoser.Input.set(e.touches[0]);
+      }, false);
+      window.addEventListener('touchmove', function(e) {
+          // we're not interested in this
+          // but prevent default behaviour
+          // so the screen doesn't scroll
+          // or zoom
+          e.preventDefault();
+      }, false);
+      window.addEventListener('touchend', function(e) {
+          // as above
+          e.preventDefault();
+      }, false);
 
       // we're ready to resize
-      NosePoser.resize();
+      NoserPoser.resize();
     },
 
-    maybeStart: function(){
-      if(NosePoser.nImages = NosePoser.nLoadedImages){
-        NosePoser.render();
+    loop: function() {
+      requestAnimFrame( NoserPoser.loop );
 
-        // listen for clicks
-        window.addEventListener('click', function(e) {
-            e.preventDefault();
-            NosePoser.Input.set(e);
-        }, false);
-        // listen for touches
-        window.addEventListener('touchstart', function(e) {
-            e.preventDefault();
-            // the event object has an array
-            // called touches, we just want
-            // the first touch
-            NosePoser.Input.set(e.touches[0]);
-        }, false);
-        window.addEventListener('touchmove', function(e) {
-            // we're not interested in this
-            // but prevent default behaviour
-            // so the screen doesn't scroll
-            // or zoom
-            e.preventDefault();
-        }, false);
-        window.addEventListener('touchend', function(e) {
-            // as above
-            e.preventDefault();
-        }, false);
-      }
+      NoserPoser.update();
+      NoserPoser.render();
     },
 
-
+    update: function() {
+    },
    
     // this is where we draw all the entities
     render: function() {
 
-      NosePoser.Draw.clear();
+      NoserPoser.Draw.clear();
 
       // draw the green bar on the bottom
-      NosePoser.Draw.rect(0,NosePoser.HEIGHT-45,NosePoser.WIDTH,45, 'green');
+      NoserPoser.Draw.rect(0,NoserPoser.HEIGHT-45,NoserPoser.WIDTH,45, 'green');
 
       // draw the face images, and a reference green rec on the face
-      NosePoser.ctx.drawImage(NosePoser.faceImg, NosePoser.faceXPos, NosePoser.faceYPos, NosePoser.faceWidth, NosePoser.faceHeight);
-    
-      NosePoser.Draw.text('Touch the nose\'s location', 9, 60, 20, '#000');
+      if (NoserPoser.imgLoaded) {
+        NoserPoser.ctx.drawImage(NoserPoser.faceImg, 0, 0, NoserPoser.WIDTH, NoserPoser.HEIGHT);
+      }
 
-      if(NosePoser.drawNose)
-        NosePoser.ctx.drawImage(NosePoser.noseImg, NosePoser.faceXPos+NosePoser.noseXOffset, NosePoser.faceYPos+NosePoser.noseYOffset,
-          NosePoser.noseWidth, NosePoser.noseHeight);
+      NoserPoser.Draw.text('Touch the nose\'s location', 9, 60, 20, '#000');
+
+      if(NoserPoser.drawNose)
+        NoserPoser.ctx.drawImage(NoserPoser.noseImg, NoserPoser.faceXPos+NoserPoser.noseXOffset, NoserPoser.faceYPos+NoserPoser.noseYOffset,
+          NoserPoser.noseWidth, NoserPoser.noseHeight);
 
     },
 
 
     resize: function() {
 
-      NosePoser.currentHeight = window.innerHeight;
+      NoserPoser.currentHeight = window.innerHeight;
       // resize the width in proportion
       // to the new height
-      NosePoser.currentWidth = NosePoser.currentHeight * NosePoser.RATIO;
+      NoserPoser.currentWidth = NoserPoser.currentHeight * NoserPoser.RATIO;
 
       // this will create some extra space on the
       // page, allowing us to scroll past
       // the address bar, thus hiding it.
-      if (NosePoser.android || NosePoser.ios) {
+      if (NoserPoser.android || NoserPoser.ios) {
         //document.body.style.height = (window.innerHeight + 50) + 'px';
       }
 
       // set the new canvas style width and height
       // note: our canvas is still 320 x 480, but
       // we're essentially scaling it with CSS
-      NosePoser.canvas.style.width = NosePoser.currentWidth + 'px';
-      NosePoser.canvas.style.height = NosePoser.currentHeight + 'px';
+      NoserPoser.canvas.style.width = NoserPoser.currentWidth + 'px';
+      NoserPoser.canvas.style.height = NoserPoser.currentHeight + 'px';
 
       // we use a timeout here because some mobile
       // browsers don't fire if there is not
@@ -173,59 +199,59 @@ angular
     }
   };
 
-	window.addEventListener('load', NosePoser.init, false);
-	window.addEventListener('resize', NosePoser.resize, false);
+	window.addEventListener('load', NoserPoser.init, false);
+	window.addEventListener('resize', NoserPoser.resize, false);
 
 	// we need to sniff out Android and iOS
 	// so that we can hide the address bar in
 	// our resize function
-	NosePoser.ua = navigator.userAgent.toLowerCase();
-	NosePoser.android = NosePoser.ua.indexOf('android') > -1 ? true : false;
-	NosePoser.ios = ( NosePoser.ua.indexOf('iphone') > -1 || NosePoser.ua.indexOf('ipad') > -1  ) ? 
+	NoserPoser.ua = navigator.userAgent.toLowerCase();
+	NoserPoser.android = NoserPoser.ua.indexOf('android') > -1 ? true : false;
+	NoserPoser.ios = ( NoserPoser.ua.indexOf('iphone') > -1 || NoserPoser.ua.indexOf('ipad') > -1  ) ? 
 	    true : false;
 
   	// abstracts various canvas operations
-	NosePoser.Draw = {
+	NoserPoser.Draw = {
 
     clear: function() {
-      NosePoser.ctx.clearRect(0, 0, NosePoser.WIDTH, NosePoser.HEIGHT);
+      NoserPoser.ctx.clearRect(0, 0, NoserPoser.WIDTH, NoserPoser.HEIGHT);
     },
 
     rect: function(x, y, w, h, col) {
-      NosePoser.ctx.fillStyle = col;
-      NosePoser.ctx.fillRect(x, y, w, h);
+      NoserPoser.ctx.fillStyle = col;
+      NoserPoser.ctx.fillRect(x, y, w, h);
     },
 
     circle: function(x, y, r, col) {
-      NosePoser.ctx.fillStyle = col;
-      NosePoser.ctx.beginPath();
-      NosePoser.ctx.arc(x + 5, y + 5, r, 0,  Math.PI * 2, true);
-      NosePoser.ctx.closePath();
-      NosePoser.ctx.fill();
+      NoserPoser.ctx.fillStyle = col;
+      NoserPoser.ctx.beginPath();
+      NoserPoser.ctx.arc(x + 5, y + 5, r, 0,  Math.PI * 2, true);
+      NoserPoser.ctx.closePath();
+      NoserPoser.ctx.fill();
     },
 
     text: function(string, x, y, size, col) {
-      NosePoser.ctx.font = 'bold '+size+'px Monospace';
-      NosePoser.ctx.fillStyle = col;
-      NosePoser.ctx.fillText(string, x, y);
+      NoserPoser.ctx.font = 'bold '+size+'px Monospace';
+      NoserPoser.ctx.fillStyle = col;
+      NoserPoser.ctx.fillText(string, x, y);
     }
 	};
 
-  NosePoser.Input = {
+  NoserPoser.Input = {
 
     x: 0,
     y: 0,
 
     set: function(data) {
-        this.x = (data.pageX - NosePoser.offset.left) / NosePoser.scale;
-        this.y = (data.pageY - NosePoser.offset.top) / NosePoser.scale;
+        this.x = (data.pageX - NoserPoser.offset.left) / NoserPoser.scale;
+        this.y = (data.pageY - NoserPoser.offset.top) / NoserPoser.scale;
 
-        NosePoser.noseXOffset = this.x - NosePoser.faceXPos;
-        NosePoser.noseYOffset = this.y - NosePoser.faceYPos;
+        NoserPoser.noseXOffset = this.x - NoserPoser.faceXPos;
+        NoserPoser.noseYOffset = this.y - NoserPoser.faceYPos;
 
-        NosePoser.drawNose = true;
+        NoserPoser.drawNose = true;
 
-        NosePoser.render();
+        NoserPoser.render();
       }
   };
 //steroids.view.setBackgroundImage("/img/space-background.png");
